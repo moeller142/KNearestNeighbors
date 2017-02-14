715dd9280e13053f7357ed0833170f7f8eecfb8e
Claudia Moeller <moeller.142@osu.edu>
2017-02-14 16:30:53 -0500

    run evaluation.py to output confusion matrixes!

--- evaluation.py
+++ evaluation.py
@@ -22,7 +22,9 @@ def confusion_matirx(reader, labels):
 	predicted = numpy.array(predicted)
 	conf_matrix = numpy.zeros((len(labels), len(labels)))
 	for actual_class, predicted_class in zip(actual, predicted):
+		#print(actual_class, predicted_class)
 		conf_matrix[conversion[actual_class]][conversion[predicted_class]] +=1
+		#print(conf_matrix)
 	return conf_matrix
 
 def income_eval(conf_matrix):
@@ -30,29 +32,34 @@ def income_eval(conf_matrix):
 	pass
 
 def error_rate(conf_matrix):
-	#error_rate = (fn+fp)/(tp + fn + fp + tn)
 	pass
-
+	#error_rate = (fn+fp)/(tp + fn + fp + tn)
 def classification_rate(conf_matrix):
 	pass
 
-def main():
+def output_file():
 	#confusion matirx, classification and error rates for test set for a few k values
 	#'ID', 'Actual Class', 'Predicted Class', 'Posterior Probability']
 	output_file = open('./Evaluation_Output', 'w')
 	output_file.write('Iris Dataset:\n\n')
 	output_file.write('Confusion Matrix\n')
 
-	labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+	labels_iris = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
 	reader = csv.DictReader(open('./iris_out.csv', 'r'))
-	iris_conf_matrix = confusion_matirx(reader, labels)
-	print(iris_conf_matrix)
-	#output_file.write(iris_conf_matrix)
+	iris_conf_matrix = confusion_matirx(reader, labels_iris)
+	output_file.write(numpy.array_str(iris_conf_matrix))
+
+	output_file.write('\n\n')
+	output_file.write('Income Dataset:\n\n')
+	output_file.write('Confusion Matrix\n')
+
+	reader = csv.DictReader(open('./income_out.csv', 'r'))
+	labels_income = [' <=50K', ' >50K']
+	income_conf_matrix = confusion_matirx(reader, labels_income)
+	output_file.write(numpy.array_str(income_conf_matrix))
 
 
 	#output_file.write('error rate = ' + error_rate + '\n\n')
 
-if __name__ == '__main__':
-	main()
 
-
+output_file()
