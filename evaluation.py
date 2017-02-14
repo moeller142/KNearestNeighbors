a1977ae64b0fa4a4a652804372f30b9e764a976b
Claudia Moeller <moeller.142@osu.edu>
2017-02-14 14:21:35 -0500

    i think were making confusion matrixes!

--- evaluation.py
+++ evaluation.py
@@ -2,47 +2,57 @@ import csv
 import knn
 import numpy
 
+#returns confusion matrix
+#params: cvs reader for input file, array of class labels
+def confusion_matirx(reader, labels):
 
-#'ID', 'Actual Class', 'Predicted Class', 'Posterior Probability']
-
-#produces confusion matrix, classificatoin, and errors rates
-def confusion_matirx(reader, output_file, labels):
-
-	#confusion matirx, classification and error rates for test set for a few k values
 	actual = []
 	predicted = []
 	for row in reader:
-		actual.add(row['Actual Class'])
-		predicted.add(row['Predicted Class']
+		actual.append(row['Actual Class'])
+		predicted.append(row['Predicted Class'])
+
+	conversion = {}
+	x = 0
+	for label in labels:
+		conversion[label] = x
+		x +=1
 
 	actual = numpy.array(actual)
 	predicted = numpy.array(predicted)
-	conf_matrix = numpy.zeros(len(labels), len(labels))
+	conf_matrix = numpy.zeros((len(labels), len(labels)))
 	for actual_class, predicted_class in zip(actual, predicted):
-		conf_matrix[actual]
+		conf_matrix[conversion[actual_class]][conversion[predicted_class]] +=1
+	return conf_matrix
 
-	output_file.write('Confusion Matrix\n\n')
-
-
-	error_rate = (fn+fp)/(tp + fn + fp + tn)
-	#classification rate???
-	output_file.write('error rate = ' + error_rate + '\n\n')
-	output_file.write('Confusion Matrix = \n')
-
-
-	input_file.close()
-
-def income_eval():
+def income_eval(conf_matrix):
 	#TP, FP, TN, FN, precision, f-measure, roc curve
+	pass
+
+def error_rate(conf_matrix):
+	#error_rate = (fn+fp)/(tp + fn + fp + tn)
+	pass
+
+def classification_rate(conf_matrix):
+	pass
 
 def main():
+	#confusion matirx, classification and error rates for test set for a few k values
+	#'ID', 'Actual Class', 'Predicted Class', 'Posterior Probability']
+	output_file = open('./Evaluation_Output', 'w')
+	output_file.write('Iris Dataset:\n\n')
+	output_file.write('Confusion Matrix\n')
+
 	labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+	reader = csv.DictReader(open('./iris_out.csv', 'r'))
+	iris_conf_matrix = confusion_matirx(reader, labels)
+	print(iris_conf_matrix)
+	#output_file.write(iris_conf_matrix)
 
-	for k in range(5, 9):
-		pass 
-		#call knn
-		#call evaluation income
-		#call evaluation iris
 
+	#output_file.write('error rate = ' + error_rate + '\n\n')
+
+if __name__ == '__main__':
+	main()
 
 
