9775278cdfeecb088c6be391eef7f65380337aa5
Claudia Moeller <moeller.142@osu.edu>
2017-02-15 11:39:42 -0500

    roc curve

--- evaluation.py
+++ evaluation.py
@@ -3,6 +3,9 @@ import numpy
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 import random
+from sklearn import metrics
+import pandas as pd
+from ggplot import *
 
 #returns confusion matrix
 #params: cvs reader for input file, array of class labels
@@ -10,11 +13,20 @@ def confusion_matirx(reader, labels):
 
 	actual = []
 	predicted = []
+
+	a = []
+	p = []
+
+	conversion = {' <=50K':0, ' >50K':1}
 	for row in reader:
 		actual.append(row['Actual Class'])
 		predicted.append(row['Predicted Class'])
 
-	#roc_curve(actual, predicted)
+		a.append(conversion[row['Actual Class']])
+		p.append(conversion[row['Predicted Class']])
+
+
+	roc_curve(a, p)
 
 	conversion = {}
 	x = 0
@@ -22,7 +34,7 @@ def confusion_matirx(reader, labels):
 		conversion[label] = x
 		x +=1
 
-	print(conversion)
+	print(conversion)			
 	actual = numpy.array(actual)
 	predicted = numpy.array(predicted)
 	conf_matrix = numpy.zeros((len(labels), len(labels)))
@@ -32,7 +44,7 @@ def confusion_matirx(reader, labels):
 	return conf_matrix
 
 def roc_curve(actual, predicted):
-	false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, predicted)
+	false_positive_rate, true_positive_rate, thresholds = metrics.roc_curve(actual, predicted)
 	roc_auc = auc(false_positive_rate, true_positive_rate)
 	plt.title('Receiver Operating Characteristic')
 	plt.plot(false_positive_rate, true_positive_rate, 'b',
@@ -45,7 +57,15 @@ def roc_curve(actual, predicted):
 	plt.xlabel('False Positive Rate')
 	plt.show()
 
-#returns accuracy of a confusion matrix
+	# fpr, tpr, _ = metrics.roc_curve(actual, predicted, pos_label = ' <=50K')
+
+	# df = pd.DataFrame(dict(fpr=fpr, tpr=tpr))	
+	# p = ggplot(df, aes(x='fpr', y='tpr')) +geom_line() +geom_abline(linetype='dashed')
+	# auc = metrics.auc(fpr,tpr)
+	# g = ggplot(df, aes(x='fpr', ymin=0, ymax='tpr')) +geom_area(alpha=0.2) +geom_line(aes(y='tpr')) +ggtitle("ROC Curve w/ AUC=%s" % str(auc))
+	# print(p)
+	# print(g)
+	#returns accuracy of a confusion matrix
 def accuracy(conf_matrix):
 	correct = 0
 	incorrect = 0
@@ -65,16 +85,16 @@ def output_file():
 	output_file.write('Iris Dataset:\n\n')
 	output_file.write('Confusion Matrix\n')
 
-	labels_iris = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-	reader = csv.DictReader(open('./iris_out.csv', 'r'))
-	iris_conf_matrix = confusion_matirx(reader, labels_iris)
-	output_file.write(numpy.array_str(iris_conf_matrix))
-	output_file.write('\n')
+	# labels_iris = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+	# reader = csv.DictReader(open('./iris_out.csv', 'r'))
+	# iris_conf_matrix = confusion_matirx(reader, labels_iris)
+	# output_file.write(numpy.array_str(iris_conf_matrix))
+	# output_file.write('\n')
 
-	acc = accuracy(iris_conf_matrix)
-	#print(acc)
-	output_file.write('accuracy = ' + str(acc) + '\n')
-	output_file.write('error rate = '+ str(1-acc))
+	# acc = accuracy(iris_conf_matrix)
+	# #print(acc)
+	# output_file.write('accuracy = ' + str(acc) + '\n')
+	# output_file.write('error rate = '+ str(1-acc))
 
 	output_file.write('\n\n')
 	output_file.write('Income Dataset:\n\n')
